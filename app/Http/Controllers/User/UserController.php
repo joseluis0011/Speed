3633041c3df3a7af7f6cd6a1e6db240ac308e904
Jhordu Huaman <huamanjhordy@gmail.com>
2018-02-17 13:12:28 -0500

    v2 create user routes

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -1,23 +1,15 @@
 <?php
 
-<<<<<<< HEAD
 namespace App\Http\Controllers\user;
-=======
 namespace App\Http\Controllers\User;
->>>>>>> origin/master
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class UserController extends Controller
 {
-<<<<<<< HEAD
-    public function index(){
-=======
     public function index()
     {
-
->>>>>>> origin/master
         return view('user.dashboard');
     }
 }
